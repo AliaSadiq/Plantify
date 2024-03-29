8852789859f296f6fa3eb58389933345f90a0171
Farwa <30295@students.riphah.edu.pk>
2024-03-29 22:02:08 +0500

    prac

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-//import logo from './logo.svg';
 import './App.css';
-import NavigationBar from './NavigationBar';
+import './index.css';
+import Footer from './components/footer';
+import NavBar from './components/nav-bar';
+import HomePage from './pages/home-page';
+import CampaignListPage from './pages/campaign-list-page';
+import CampaignDetailPage from './pages/campaign-detail-page'; // Import CampaignDetailPage component
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar/>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+          <Route index element={<HomePage/>}/>
+          <Route path="campaign-detail" element={<CampaignDetailPage/>} />
+          <Route path="campaign" element={<CampaignListPage/>} />
+      </Routes>
+      {/* <Footer/> */}
+    </BrowserRouter>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+          'nav-green': '#dbe0d1',
+          'light-black': "#222222",
+          'sage-200': "#bac4a7",
+          'sage-100': "#F2F4EF"
+        },
+        fontFamily: {
+          josefins: ["Josefin Sans", "sans-serif"],
+        }
+    },
   },
   plugins: [],
 }
