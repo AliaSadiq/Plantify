96bc05e92165117f9e4dda57ad1f50471511dbd2
shabeeh200 <shabeehfatima@gmail.com>
2024-05-16 21:53:07 +0500

    Social Profile Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,30 +15,39 @@ import SocialSignUpPage from './pages/social-signup-page';
 import SettingsPage from './pages/settings';
 import AuthCheck from './pages/check';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Profile from './profileComp/profile-full';
 import React from 'react';
 
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <NavBar/>
+//       <Routes>
+//           <Route index element={<LandingPage/>}/>
+//           <Route path="campaign" element={<CampaignPage/>} />
+//           {/* <Route path="campaign/socialGroup/:id" element={<SocialProfilePage/>} /> */}
+//           <Route path="home" element={<HomePage/>} />
+//           <Route path="login" element={<LoginPage/>} />
+//           <Route path="logout" element={<Logout/>} />
+//           <Route path="shop" element={<Shop/>} />
+//           <Route path="plantify-network" element={<PlantifyNetwork/>} />
+//           <Route path="contact-us" element={<ContactUsPage/>} />
+//           <Route path="about-us" element={<PlantifyNetwork/>} />
+//           <Route path="social-signUp" element={<SocialSignUpPage/>} />
+//           <Route path="settings" element={<SettingsPage/>} />
+//           <Route path='auth-check' element={<AuthCheck/>} />
+//       </Routes>
+//       <Footer/>
+//     </BrowserRouter>
+//   );
+// }
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar/>
-      <Routes>
-          <Route index element={<LandingPage/>}/>
-          <Route path="campaign" element={<CampaignPage/>} />
-          {/* <Route path="campaign/socialGroup/:id" element={<SocialProfilePage/>} /> */}
-          <Route path="home" element={<HomePage/>} />
-          <Route path="login" element={<LoginPage/>} />
-          <Route path="logout" element={<Logout/>} />
-          <Route path="shop" element={<Shop/>} />
-          <Route path="plantify-network" element={<PlantifyNetwork/>} />
-          <Route path="contact-us" element={<ContactUsPage/>} />
-          <Route path="about-us" element={<PlantifyNetwork/>} />
-          <Route path="social-signUp" element={<SocialSignUpPage/>} />
-          <Route path="settings" element={<SettingsPage/>} />
-          <Route path='auth-check' element={<AuthCheck/>} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <div className="App bg-black">
+    
+    <Profile/> 
+
+    </div>
   );
 }
-
 export default App;
