84966feafd5cfe6d9bf5c9a12b2099e5db293ee7
AliaSadiq <aliasadiq2003@gmail.com>
2024-05-17 02:10:06 +0500

    Initial empty commit

--- backend/routes/socialgroup.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {createSocialGroup, getSocialGroup} = require( '../controllers/socialgroup.controller.js');
-
-router.post("/", createSocialGroup);
-router.get("/:id", getSocialGroup);
-
-module.exports = router;
--- backend/routes/testimonial.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {createTestimonial, getTestimonial, getTestimonials} = require( '../controllers/testimonial.controller.js');
-
-router.post("/", createTestimonial);
-router.get("/:id", getTestimonial);
-router.get("/", getTestimonials);
-
-module.exports = router;
