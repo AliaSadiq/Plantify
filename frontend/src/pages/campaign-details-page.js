b4726fcf791cc15aab8c1b2a51f08c0bd32c6978
farwa <farwa.tariq.ahmed@gmail.com>
2024-07-19 12:56:42 +0500

    campaign page

--- frontend/src/pages/campaign-details-page.js
+++ frontend/src/pages/campaign-details-page.js
@@ -54,27 +54,32 @@ export default function CampaignDetailsPage() {
                 {/* Wider Div */}
                 <div className="w-full md:w-[60%] lg:w-[55%] bg-white p-4 rounded-[20px] shadow-md">
                     <div className="relative">
-                        <div className="absolute flex flex-row gap-2 items-center justify-center top-4 right-4">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="size-6"
-                            >
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                            </svg>
-                            <svg 
-                                xmlns="http://www.w3.org/2000/svg" 
-                                fill="none" viewBox="0 0 24 24" 
-                                strokeWidth={1.5} 
-                                stroke="currentColor" 
-                                className="size-6"
-                            >
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
-                            </svg>
+                        <div className="absolute flex flex-row gap-2 items-center justify-center top-4 right-4 p-2 rounded-full bg-navygreen-100">
+                            <div className="p-2 rounded-full hover:bg-navygreen-200">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                </svg>
+                            </div>
+                            <div className="p-2 rounded-full hover:bg-navygreen-200">
+                                <svg 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    fill="none" viewBox="0 0 24 24" 
+                                    strokeWidth={1.5} 
+                                    stroke="currentColor" 
+                                    className="w-6 h-6"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                                </svg>
+                            </div>
                         </div>
+
 
                         <img className="w-full h-[500px] object-cover rounded-[20px]" src={`/assets/${campaign.image}`}></img>
                         <div className="absolute left-4 bottom-4 w-[40%] p-4 bg-white rounded-[20px]">
