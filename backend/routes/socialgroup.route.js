7395838fb9ab9e177636218589faa03f1d0ffeac
feline-fine <farwa.tariq.ahmed@gmail.com>
2024-09-22 16:12:00 +0500

    seller signup

--- backend/routes/socialgroup.route.js
+++ backend/routes/socialgroup.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {createSocialGroup, getSocialGroup, getSocialGroupsAccepted, getSocialGroupsOnWait, updateSocialGroup, deleteSocialGroup, getSocialGroupByUserId} = require( '../controllers/socialgroup.controller.js');
+const {createSocialGroup, getSocialGroup, getSocialGroupsAccepted, getSocialGroupsOnWait, updateSocialGroup, deleteSocialGroup, getSocialGroupByUserId, getSocialGroupCount} = require( '../controllers/socialgroup.controller.js');
 
 router.post("/", createSocialGroup);
 router.get("/accepted", getSocialGroupsAccepted);
+router.get("/count", getSocialGroupCount);
 router.get("/onWait", getSocialGroupsOnWait);
 router.get("/:id", getSocialGroup);
 router.put("/:id", updateSocialGroup);
