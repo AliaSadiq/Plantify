322d0b49cfc9f56a61884a3cca582da8f645ea72
Alia Sadiq <130149083+AliaSadiq@users.noreply.github.com>
2024-12-09 21:26:09 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,12 @@ const limiter = rateLimit({
 app.use('/api/user', limiter);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ["https://plantify-webapp.vercel.app","https://plantify-backend.vercel.app"],
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  // credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // routes
 app.use("/api/campaigns", campaignRoute);
