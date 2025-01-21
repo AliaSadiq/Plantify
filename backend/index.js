e97e18a483280ffff6eb97a196b23688b5a08c9c
Alia Sadiq <130149083+AliaSadiq@users.noreply.github.com>
2025-01-22 02:06:27 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -139,7 +139,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 const corsOptions = {
   origin: process.env.NODE_ENV === "production" 
     ? process.env.FRONTEND_URL 
-    : ["http://localhost:3000", process.env.FRONTEND_URL], 
+    : ["http://localhost:3000", process.env.FRONTEND_URL,"https://plantify-backend.vercel.app"], 
   methods: ["POST", "GET", "PUT", "DELETE"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true, 
